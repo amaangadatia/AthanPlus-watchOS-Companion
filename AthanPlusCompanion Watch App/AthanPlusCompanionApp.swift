3bc0c8d0e7c5728f8557b6db9108f819017d2325
amaangadatia <amaangadatia2k@gmail.com>
2024-09-09 16:23:06 -0400

    Initial Commit

---
+++ AthanPlusCompanion Watch App/AthanPlusCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthanPlusCompanionApp.swift
+//  AthanPlusCompanion Watch App
+//
+//  Created by Amaan Gadatia on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct AthanPlusCompanion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
